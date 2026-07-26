eab769ce8d6f4957d2f30cf5d60606ddb878cf8a
aizhari <121381075+aizhari1@users.noreply.github.com>
2026-07-26 10:26:33 +0300

    Update app.config.ts

--- apps/api/src/config/app.config.ts
+++ apps/api/src/config/app.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   env: process.env.NODE_ENV || 'development',
-  port: parseInt(process.env.PORT || '4000', 10),
+  port: parseInt(process.env.PORT || '3000', 10),
   apiPrefix: process.env.API_PREFIX || 'api/v1',
   appUrl: process.env.APP_URL || 'http://localhost:4000',
   clientUrl: process.env.CLIENT_URL || 'http://localhost:3000',
